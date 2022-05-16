05bad8c03d39c50b88d4436f6e85beb658203378
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-05-16 22:30:09 +0000

    req 6

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT 
+    ROUND(MIN(plan_value), 2) AS faturamento_minimo,
+    MAX(plan_value) AS faturamento_maximo,
+    ROUND(AVG(plan_value), 2) AS faturamento_medio,
+    ROUND(SUM(plan_value), 2) AS faturamento_total
+FROM
+    SpotifyClone.plans AS p
+    INNER JOIN SpotifyClone.users AS u ON u.plan = p.plan_id
